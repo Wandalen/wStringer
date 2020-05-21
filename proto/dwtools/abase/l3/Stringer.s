9940ee887fd8c07809db8181832aa51af5d2996c
wandalen <wandalen.me@gmail.com>
2020-05-21 17:58:07 +0300

    .

--- proto/dwtools/abase/l3/Stringer.s
+++ proto/dwtools/abase/l3/Stringer.s
@@ -1928,7 +1928,7 @@ _.mapExtend( Self, Proto );
 // export
 // --
 
-if( typeof module !== 'undefined' && module !== null )
+if( typeof module !== 'undefined' )
 module[ 'exports' ] = Self;
 
 })();
