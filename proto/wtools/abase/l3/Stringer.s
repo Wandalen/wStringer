780233467e8a1c292242601d0618f7d75cf554ff
wandalen <wandalen.me@gmail.com>
2020-07-21 12:06:55 +0300

    .

--- proto/wtools/abase/l3/Stringer.s
+++ proto/wtools/abase/l3/Stringer.s
@@ -1253,7 +1253,7 @@ function _toStrFromStr( src, o )
 
   if( o.limitStringLength )
   {
-    result = _.strStrShort({ src, limit : o.limitStringLength, wrap : q, escaping : 1 });
+    result = _.strStrShort({ src, limit : o.limitStringLength, prefix : q, postfix : q, onEscape : 1 });
     if( result.length > o.limitStringLength )
     {
       result = '[ ' + result + ' ]';
