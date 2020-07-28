691d82b466d55f6fe26f8986cf404c3ff2a4bffd
Jackie <jackshel45@gmail.com>
2020-07-28 08:55:07 +0300

    minor

--- proto/wtools/abase/l3/Stringer.s
+++ proto/wtools/abase/l3/Stringer.s
@@ -808,7 +808,7 @@ function _toStrShort( src, o )
       var o2 =
       {
         limitStringLength : o.limitStringLength ? Math.min( o.limitStringLength, 40 ) : 40,
-        stringWrapper : o.stringWrapper,
+        stringWrapper : o.stringWrapper ? o.stringWrapper : '',
         prefix : o.prefix,
         postfix : o.postfix,
         infix : o.infix
